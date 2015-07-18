bb59210ed01e634e0c019b7e5ace4c758c911b8d
xSmurf <xsmurf+github@smurfturf.net>
2015-07-18 00:09:52 +0000

    Moved root permissions check out of config for reuse in profiles

--- config.go
+++ config.go
@@ -2,11 +2,8 @@ package oz
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
-	"path"
-	"syscall"
 )
 
 type Config struct {
@@ -66,28 +63,4 @@ func LoadConfig(cpath string) (*Config, error) {
 		return nil, err
 	}
 	return c, nil
-}
-
-func checkConfigPermissions(fpath string) error {
-	pd := path.Dir(fpath)
-	for _, fp := range []string{pd, fpath} {
-		if err := checkPathRootPermissions(fp); err != nil {
-			return fmt.Errorf("file (%s) is %s", fp, err)
-		}
-	}
-	return nil
-}
-
-func checkPathRootPermissions(fpath string) error {
-	fstat, err := os.Stat(fpath)
-	if err != nil {
-		return err
-	}
-	if (fstat.Mode().Perm() & syscall.S_IWOTH) != 0 {
-		return fmt.Errorf("writable by everyone!", fpath)
-	}
-	if (fstat.Mode().Perm()&syscall.S_IWGRP) != 0 && fstat.Sys().(*syscall.Stat_t).Gid != 0 {
-		return fmt.Errorf("writable by someone else than root!", err)
-	}
-	return nil
 }

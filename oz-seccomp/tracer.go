5b9d6797b421f1c3c3198e1d2108237d1a419f38
dma <dma@subgraph.com>
2015-09-24 13:32:41 -0400

    More refactoring, committing this so Oz builds.

--- oz-seccomp/tracer.go
+++ oz-seccomp/tracer.go
@@ -2,19 +2,14 @@ package seccomp
 
 import (
 	"bytes"
-	"encoding/binary"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/subgraph/oz"
 	"golang.org/x/sys/unix"
 	"io"
-	"io/ioutil"
 	"os"
 	"os/exec"
-	"strconv"
 	"syscall"
-	"unsafe"
 )
 
 const (
@@ -93,13 +88,11 @@ func Tracer() {
 				}
 				continue
 			}
-/*
-		        if s.Signaled() == true {
-			        log.Error("Other pid signalled %v %v", pid, s) 
+			if s.Signaled() == true {
+				log.Error("Other pid signalled %v %v", pid, s)
 				delete(children, pid)
-																			                                        continue
-																								                                }
-*/
+				continue
+			}
 			switch uint32(s) >> 8 {
 
 			case uint32(unix.SIGTRAP) | (unix.PTRACE_EVENT_SECCOMP << 8):
@@ -115,10 +108,21 @@ func Tracer() {
 				}
 
 				systemcall, err := syscallByNum(getSyscallNumber(regs))
-
 				if err != nil {
 					log.Error("Error: %v", err)
 					continue
+				}
+
+				/* TEMPORARY */
+
+				if getSyscallNumber(regs) == 21 {
+					str, err := readStringArg(pid, uintptr(regs.Rdi))
+					if err == nil {
+						log.Info(render_access(pid, str, int(regs.Rsi)))
+					} else {
+						log.Info("error %v", err)
+					}
+
 				}
 				log.Info(renderSyscallBasic(pid, systemcall, regs))
 				continue
@@ -161,109 +165,4 @@ func Tracer() {
 		}
 	}
 
-}
-
-
-func readStringArg(pid int, addr uintptr) (s string, err error) {
-	buf := make([]byte, unsafe.Sizeof(addr))
-	done := false
-	err = nil
-	for done == false {
-		_, err := syscall.PtracePeekText(pid, addr, buf)
-		if err != nil {
-			fmt.Printf("Error (ptrace): %v\n", err)
-		} else {
-			for b := range buf {
-				if buf[b] == 0 {
-					done = true
-					break
-				} else {
-					s += string(buf[b])
-					if len(s) > 90 {
-						s += "..."
-						break
-					}
-				}
-			}
-		}
-		addr += unsafe.Sizeof(addr)
-	}
-	return s, nil
-}
-
-func readUIntArg(pid int, addr uintptr) (uint64, error) {
-	buf := make([]byte, unsafe.Sizeof(addr))
-
-	_, err := syscall.PtracePeekText(pid, addr, buf)
-	if err != nil {
-		fmt.Printf("Error (ptrace): %v\n", err)
-		return 0, err
-	} else {
-		i := binary.LittleEndian.Uint64(buf)
-		return i, nil
-	}
-	return 0, errors.New("Error.")
-}
-
-func readPtrArg(pid int, addr uintptr) (uintptr, error) {
-
-	buf := make([]byte, unsafe.Sizeof(addr))
-
-	_, err := syscall.PtracePeekText(pid, addr, buf)
-	if err != nil {
-		fmt.Printf("Error (ptrace): %v\n", err)
-		return 0, err
-	} else {
-		i := binary.LittleEndian.Uint64(buf)
-		return uintptr(i), nil
-	}
-	return 0, nil
-}
-
-func syscallByNum(num int) (s SystemCall, err error) {
-	var q SystemCall = SystemCall{"", "", -1, []int{0, 0, 0, 0, 0, 0}}
-	for i := range syscalls {
-		if syscalls[i].num == num {
-			q = syscalls[i]
-			return q, nil
-		}
-	}
-	return q, errors.New("System call not found.\n")
-}
-
-func syscallByName(name string) (s SystemCall, err error) {
-	var q SystemCall = SystemCall{"", "", -1, []int{0, 0, 0, 0, 0, 0}}
-	for i := range syscalls {
-		if syscalls[i].name == name {
-			q = syscalls[i]
-			return q, nil
-		}
-	}
-	return q, errors.New("System call not found.\n")
-}
-
-func isPrintableASCII(s string) bool {
-	for _, x := range s {
-		if x > 127 {
-			return false
-		}
-	}
-	return true
-}
-
-func getProcessCmdLine(pid int) string {
-	path := "/proc/" + strconv.Itoa(pid) + "/cmdline"
-	cmdline, err := ioutil.ReadFile(path)
-	for b := range cmdline {
-		if b <= (len(cmdline) - 1) {
-			if cmdline[b] == 0x00 {
-				cmdline[b] = 0x20
-			}
-		}
-	}
-	if err != nil {
-		log.Error("Error (read): %v", err)
-		return "unknown"
-	}
-	return string(cmdline)
 }

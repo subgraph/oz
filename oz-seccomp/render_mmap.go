08b73ce1eb873f45f34e20121b1ef980fe3cd1b1
dma <dma@subgraph.com>
2015-09-24 18:31:12 -0400

    Position key flag to be first in list.

--- oz-seccomp/render_mmap.go
+++ oz-seccomp/render_mmap.go
@@ -14,8 +14,6 @@ func render_mmap(pid int, args RegisterArgs) (string, error) {
 	}
 
 	mmapflags := map[uint]string{
-		syscall.MAP_SHARED:     "MAP_SHARED",
-		syscall.MAP_PRIVATE:    "MAP_PRIVATE",
 		syscall.MAP_32BIT:      "MAP_32BIT",
 		syscall.MAP_ANONYMOUS:  "MAP_ANONYMOUS",
 		syscall.MAP_EXECUTABLE: "MAP_EXECUTABLE",
@@ -37,6 +35,7 @@ func render_mmap(pid int, args RegisterArgs) (string, error) {
 	mmapflagsval := args[3]
 
 	protflagstr := ""
+	mmapflagstr := ""
 
 	if mode == syscall.PROT_NONE {
 		protflagstr = "PROT_NONE"
@@ -44,9 +43,20 @@ func render_mmap(pid int, args RegisterArgs) (string, error) {
 		protflagstr = renderFlags(protflags, uint(mode))
 	}
 
-	mmapflagstr := renderFlags(mmapflags, uint(mmapflagsval))
+
+	if (mmapflagsval & syscall.MAP_PRIVATE) == syscall.MAP_PRIVATE {
+		mmapflagstr += "MAP_PRIVATE"
+	} else if (mmapflagsval & syscall.MAP_SHARED) == syscall.MAP_SHARED {
+		mmapflagstr += "MAP_SHARED"
+	}
+
+	tmp := renderFlags(mmapflags, uint(mmapflagsval))
+	if (tmp != "") {
+		mmapflagstr += "|"
+		mmapflagstr += tmp
+	}
 
 	callrep := fmt.Sprintf("mmap(0x%X, %d, %s, %s, %d, %d)", uintptr(args[0]), args[1], protflagstr, mmapflagstr, args[4], args[5])
 
-	return fmt.Sprintf("==============================================\nseccomp hit on sandbox pid %v (%v) syscall %v (%v): \n\n%s %d %d\nI ==============================================\n\n", pid, getProcessCmdLine(pid), "mmap", syscall.SYS_MMAP, callrep, mode, mmapflagsval), nil
+	return fmt.Sprintf("==============================================\nseccomp hit on sandbox pid %v (%v) syscall %v (%v): \n\n%s\nI ==============================================\n\n", pid, getProcessCmdLine(pid), "mmap", syscall.SYS_MMAP, callrep), nil
 }

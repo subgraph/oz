a05c74cdd34dbba8c83a53453b53d88010118149
dma <dma@subgraph.com>
2015-09-24 16:10:25 -0400

    Some utility and support functions for rendering system call invocations in Oz seccomp tracer.

--- oz-seccomp/render_syscall.go
+++ oz-seccomp/render_syscall.go
@@ -4,11 +4,29 @@ import (
 	"syscall"
 )
 
-type RenderingFunctions map[int]func(int, RegisterArgs) (string, error) 
+type RenderingFunctions map[int]func(int, RegisterArgs) (string, error)
 
 func getRenderingFunctions() RenderingFunctions {
-	r := map[int]func(pid int, args RegisterArgs) (string, error) { 
-		syscall.SYS_ACCESS : render_access,
+	r := map[int]func(pid int, args RegisterArgs) (string, error){
+		syscall.SYS_ACCESS:   render_access,
+		syscall.SYS_MPROTECT: render_mprotect,
 	}
 	return r
+}
+
+func renderFlags(flags map[uint]string, val uint) string {
+	found := false
+	flagstr := ""
+
+	for flag := range flags {
+		if (val & uint(flag)) == val {
+			if found == true {
+				flagstr += "|"
+			}
+			flagstr += flags[flag]
+			found = true
+		}
+	}
+	return flagstr
+
 }

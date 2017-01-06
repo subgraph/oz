cddb0c26d75a203189417d402fc6b54357edf61b
dma <dma@subgraph.com>
2017-01-06 22:39:41 +0000

    Protocol enhancements from master

--- oz-init/protocol.go
+++ oz-init/protocol.go
@@ -24,10 +24,17 @@ type RunProgramMsg struct {
 	Path string
 }
 
+type ForwarderSuccessMsg struct {
+	Port string "ForwarderSuccess"
+	Proto string
+	Addr string
+}
+
 var messageFactory = ipc.NewMsgFactory(
 	new(OkMsg),
 	new(ErrorMsg),
 	new(PingMsg),
 	new(RunShellMsg),
 	new(RunProgramMsg),
+	new(ForwarderSuccessMsg),
 )
